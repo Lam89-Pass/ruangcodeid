342c501b6fda29b803629be185fb6b6feabb6a49
Alam <aaallaaamm03@gmail.com>
2026-04-28 10:08:49 +0700

    Push Ke 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
-import HeroSection from '@/components/home/HeroSection';
-import AboutSection from '@/components/home/AboutSection';
-import FaqSection from '@/components/shared/FaqSection';
-// import Testimoni from '@/components/home/Testimoni'; // Buka komentar ini saat komponennya sudah dibuat
+import HeroSection from "@/components/home/HeroSection";
+import AboutSection from "@/components/home/AboutSection";
+import PricingSection from "@/components/home/PricingSection";
+import TestimoniSection from "@/components/shared/TestimoniSection";
+import FaqSection from "@/components/shared/FaqSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <AboutSection />
-
-      
+      <PricingSection />
+      <TestimoniSection />
       <FaqSection />
     </>
   );
